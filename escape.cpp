7fa78cf60a0695448eb48846ec53648f0e0d1707
weddingmagicallen-star <weddingmagicallen@gmail.com>
2026-04-22 15:24:42 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+  cout<< "1快樂星球" <<endl;
+  cout<< "2快樂星球" <<endl;
+  cout<< "3快樂/t星球" <<endl;
+  cout<< "4快樂星/n球" <<endl;
+  cout<< "//5快樂星球//" <<endl;
+  cout<< "//6快樂星球//" <<endl;
+  return 0;
+}
